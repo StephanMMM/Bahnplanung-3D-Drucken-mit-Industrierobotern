99faf05fc53d9651eb7973f437c88d7acd733bfd
RiSKeD <34069925+RiSKeD@users.noreply.github.com>
2017-11-28 18:50:24 +0100

    Added Comments

--- Programm.cs
+++ Programm.cs
@@ -13,32 +13,46 @@ namespace Werkzeugbahnplanung
         {
 
         }
-
+        /*Funktion für eine Input-Textdatei
+          entsprechend der Einigung
+          1. Zeile : Grundwerte Voxelmodell
+          2.-N. Zeile: Voxel
+          Als Ausgabewert wird das fertige Voxelmodell
+          zurückgegeben
+        */
         public Voxelmodell Input(string filename)
         {
+            //Erstelle Dateipfad und checke ob Datei vorhanden
             var path = Path.Combine(Directory.GetCurrentDirectory(), "\\");
             path += filename;
             if (!File.Exists(path)) { return null; }
-            string[] lines = System.IO.File.ReadAllLines(@filename);
+            //Speichere Zeile für Zeile die Daten ab
+            string[] lines = System.IO.File.ReadAllLines(@path);
+            //
             string[] tokens = lines[0].Split(',');
             int boundB_x = 0, boundB_y = 0, boundB_z = 0, anzSchichten = 0;
-            Voxel[,,] voxelmatrix = new Voxel[boundB_x, boundB_y, boundB_z];
             List<List<Voxel>> schichten = new List<List<Voxel>>();
+            //Versuche Werte aus der 1. Zeile zu parsen ansonsten Abbruch
             if (Int32.TryParse(tokens[0], out boundB_x) &&
                 Int32.TryParse(tokens[1], out boundB_y) &&
                 Int32.TryParse(tokens[2], out boundB_z) &&
                 Int32.TryParse(tokens[3], out anzSchichten))
             {
+                //Erstelle für jede Schicht eine eigene Liste
                 for (int i = 0; i < anzSchichten; i++)
                 {
                     schichten.Add(new List<Voxel>());
                 }
+                //Erstelle Voxelmatrix entsprechend den Dimensionen des Modells
+                Voxel[,,] voxelmatrix = new Voxel[boundB_x, boundB_y, boundB_z];
+                //Schleife über alle Zeilen(bis auf die erste)
                 for (int i = 1; i < lines.Length; i++)
                 {
                     string[] voxelparam = lines[i].Split(',');
                     ushort[] voxelKoords = new ushort[3];
                     int schicht = 0;
                     bool schichtrand = false, modellrand = false;
+                    //Versuche alle Werte der Zeile zu parsen ansonsten gehe zur nächsten Zeile
                     if (ushort.TryParse(voxelparam[0], out voxelKoords[0]) &&
                         ushort.TryParse(voxelparam[1], out voxelKoords[1]) &&
                         ushort.TryParse(voxelparam[2], out voxelKoords[2]) &&
@@ -46,6 +60,8 @@ namespace Werkzeugbahnplanung
                         bool.TryParse(voxelparam[4], out modellrand) &&
                         Int32.TryParse(voxelparam[5], out schicht))
                     {
+                        //Erstelle anhand der Werte einen neuen Voxel in der Matrix 
+                        //und in der entsprechenden Schicht
                         voxelmatrix[voxelKoords[0], voxelKoords[1], voxelKoords[2]] = new Voxel(schichtrand, modellrand, voxelKoords[0], voxelKoords[1], voxelKoords[2]);
                         schichten[i].Add(voxelmatrix[voxelKoords[0], voxelKoords[1], voxelKoords[2]]);
                     }
@@ -54,6 +70,7 @@ namespace Werkzeugbahnplanung
                         continue;
                     }
                 }
+                //Erstellt Voxelmodell wenn Datei vollkommen durchlaufen und gebe diese zurück
                 Voxelmodell voxelmodell = new Voxelmodell(anzSchichten, boundB_x, boundB_y, boundB_z, voxelmatrix, schichten);
                 return voxelmodell;
             }
@@ -61,7 +78,6 @@ namespace Werkzeugbahnplanung
             {
                 return null;
             }
-
         }
     }
 }
