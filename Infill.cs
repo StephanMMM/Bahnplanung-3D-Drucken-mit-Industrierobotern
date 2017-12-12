61ff8a0a8da470b77c940aa5653333e2dd7467a5
Eharez <rath.lennard@gmail.com>
2017-12-12 08:50:03 +0100

    Add files via upload

---
+++ Infill.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Werkzeugbahnplanung
+{
+    class Infill
+    {
+        private int[,,] infill_baseCell;
+        private int infill_density;
+        private int infill_offset;
+        private string infill_type;
+
+        public int[,,] BaseCell { get => infill_baseCell; set => infill_baseCell = value; }
+        public int Density { get => infill_density; set => infill_density = value; }
+        public int Offset { get => infill_offset; set => infill_offset = value; }
+
+        public Infill(int density, string type, int offset = 0) {
+            if (density != 0)
+            {
+                infill_density = (100 / density + 1) / 2;
+                infill_density *= 2;
+                infill_baseCell = Generate_3DInfill();
+                infill_type = "Is_" + type;
+                if (type == "3DInfill")
+                {
+                    Generate_3DInfill();
+                }
+                if (type == "HexInfill")
+                {
+                    Generate_HexInfill();
+                }
+            }
+            else {
+                infill_type = "Is_Empty";
+            }
+
+        }
+
+        public int IsInfill(int x, int y=0, int z=0) {
+            //prone to errors do not change this and keep return value and parameters for Is_ Methodes equal
+            Object[] param = new Object[] { x, y, z };
+            return Int32.Parse(typeof(Infill).GetMethod(infill_type).Invoke(this, param).ToString());
+
+        }
+
+        public int[,,] Generate_3DInfill()
+        {
+            int[,,] Sample = new int[2 * infill_density, infill_density, 2 * infill_density];
+            //Definition der Einzel Zelle
+            //Bottom Half
+            for (int depth = 0; depth <= infill_density / 2; ++depth)
+            {
+                for (int width = infill_density / 2 + depth; width < infill_density + infill_density / 2 - depth - 1; ++width)
+                {
+                    Sample[width, depth, 0] = 1;//Bottom flat triangle
+                }
+            }
+            for (int height = 0; height < infill_density / 2; ++height)
+            {
+                for (int depth = 0; depth <= infill_density / 2 - height; ++depth)
+                {
+                    Sample[infill_density - 1, infill_density / 2 + depth + height-1, height] = 1;//Bottom standing triangle 
+                    Sample[infill_density*2-1, depth, infill_density - height] = 1;//right side standing triangle 
+                }
+                
+                for (int depth = 0; depth < infill_density / 2 + height; ++depth)//faces
+                {
+                    Sample[infill_density / 2 - height + depth, depth, height ] = 1;
+                    Sample[infill_density + infill_density / 2 + height - 1 - depth-1, depth, height] = 1;//lower half
+
+                    Sample[infill_density / 2 + height - depth - 1, infill_density - depth - 2, infill_density - height-1] = 1;
+                    Sample[infill_density + infill_density / 2 - height + depth-1, infill_density - depth-2, infill_density - height-1] = 1;//upper half
+                }
+            }
+            for (int height = 0; height <= infill_density / 2; ++height)
+            {
+                for (int width = infill_density-height-1; width <= infill_density + height; ++width)
+                {
+                    Sample[width, infill_density - 2, infill_density / 2 + height] = 1;//line
+                }
+            }
+            for (int width = 0; width < infill_density / 2; ++width)
+            {
+                for (int depth = 0; depth < infill_density / 2 - width; ++depth)
+                {
+                    Sample[width, infill_density - 1 - depth, infill_density] = 1;//Mid flat Triangle
+                    Sample[2 * infill_density - 1 - width, infill_density - 2 - depth, infill_density] = 1;
+                }
+            }
+            //return Sample;
+            for (int i = 0; i < infill_density; i++)//mirror the lower half to the upper half
+            {
+                for (int y = 0; y < infill_density; ++y)
+                {
+                    for (int x = 0; x < 2*infill_density; ++x)
+                    {
+                        Sample[x, y, 2 * infill_density - 1 - i] = Sample[x, y, i + 1];
+                    }
+                }
+            }
+            return Sample;
+        }
+
+        public int Is_3DInfill(int x, int y, int z)
+        {
+            Boolean isEven = (0 == (y/(infill_density-1))% 2);
+            y = y % (infill_density-1);
+            x = x % (2*infill_density);
+            z = z % (2*infill_density);
+            if (!isEven)
+            {
+                x += (infill_density);
+                z += (infill_density);
+                x = x % (2 * infill_density);
+                z = z % (2 * infill_density);
+            }
+            return BaseCell[x,y,z];
+        }
+        
+
+        public int[,,] Generate_HexInfill()
+        {
+            int half_density = infill_density / 2;
+            int[,,] Sample = new int[(infill_density+half_density), 2*infill_density , 1];
+            //Definition der Einzel Zelle
+            for (int width = 0; width < infill_density; width++)
+            {
+                Sample[half_density + width, 0, 0] = 1;
+            }
+            for (int width = 0; width < half_density; width++)
+            {
+                Sample[width, (half_density-width - 1) * 2, 0] = 1;
+                Sample[width, (half_density - width - 1) * 2 + 1 , 0] = 1;
+                Sample[width, 2*infill_density-((half_density - width - 1) * 2 + 1), 0] = 1;
+                Sample[width, 2 * infill_density - ((half_density - width - 1) * 2 + 2), 0] = 1;
+            }
+            return Sample;
+        }
+
+        private int Is_HexInfill(int x, int y, int z)
+        {
+            Boolean isEven = (0 == (x / (infill_density + (infill_density / 2) - 1)) % 2);
+            y = y % (2 * infill_density - 1);
+            x = x % (infill_density + (infill_density / 2) - 1);
+            if (!isEven)
+            {
+                y += (infill_density - 1);
+                y = y % (2 * infill_density - 1);
+            }
+            return BaseCell[x, y, 0];
+        }
+        private int Is_LineInfill(int x, int y, int z)
+        {
+            if (x % (infill_density * 2) == infill_offset || y % (infill_density * 2) == infill_offset) {
+                return 1;
+            }
+            return 0;
+        }
+
+        private int Is_Line3DInfill(int x, int y, int z)
+        {
+            //figure out how too turn offset in ° -> 45° tilt:
+            if (infill_offset != 0) {
+                if (0 == (z+y+(infill_density*4-x)) % (infill_density * 4)) {
+                    return 1;
+                }
+                int plane0011 = z + y + x;
+                int plane0001 = z + x + (infill_density * 4 - y);
+                x = y + x +(infill_density * 4 - z);
+                y = plane0011;
+                z = plane0001;
+                
+            }
+            if (x % (infill_density * 4) == 0 || y % (infill_density * 4) == 0 || z % (infill_density * 4) == 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public int Is_Empty(int x, int y, int z)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Voxelmodell.cs
@@ -0,0 +1,177 @@
+using Werkzeugbahnplanung;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkzeugbahnplanung
+{
+    public class Voxelmodell
+    {
+        private int m_AnzahlSchichten;
+        private Infill m_Boundingbox;
+        //3-D Voxelmodell
+        private Voxel[,,] m_Voxelmatrix;
+        //Liste auf die entsprechenden Schichten-Listen
+        private List<List<Voxel>> m_Schichten;
+
+        //Konstruktor für Input-Funktion vorgesehen
+        public Voxelmodell(int anzahlSchichten,Voxel[,,] voxelmatrix, List<List<Voxel>> schichten, int infillDensity = 20, string infillType = "3DInfill")
+        {
+            m_AnzahlSchichten = anzahlSchichten;
+            m_Boundingbox = new Infill(infillDensity, infillType);
+            m_Voxelmatrix = voxelmatrix;
+            m_Schichten = schichten;
+        }
+        /// <summary>
+        /// lediglich benötigt, um die Randverbreiterung einfacher zu testen
+        /// </summary>
+        /// <returns></returns>
+        public Voxel[,,] getVoxelmatrix()
+        {
+            return m_Voxelmatrix;
+        }
+         public List<List<Voxel>> Schichten()
+        {
+            return m_Schichten;
+        }
+
+        #region InsertInfill
+        /*Funktion, die eine Boundingbox eines Infill-Musters
+          (derselben Größe(!)) mit dem Voxelmodell merged.
+          Bounding-Box : true = Voxel gesetzt im Infill */
+        public void InsertInfill()
+        {
+            ushort[] koords = new ushort[3];
+            //Schleifen die über alle Voxel des Modells gehen
+            foreach (List<Voxel> schicht in m_Schichten)
+            {
+                for (int i = 0; i < schicht.Count(); i++)
+                {
+                    //Voxel die Teil des Randes sind kommen nicht in Frage
+                    if (schicht[i].getSchichtrand() != true) //#Question: Modellrand anstatt Schichtrand?
+                    {
+                        koords = schicht[i].getKoords();
+                        //Falls kein Infill an Stelle des Voxels, lösche diesen
+                        //aus unserem Voxelmodell
+                        if (0 == m_Boundingbox.IsInfill(koords[0], koords[1], koords[2]))
+                        {
+                            m_Voxelmatrix[koords[0], koords[1], koords[2]] = null;
+                            schicht.Remove(schicht[i]);
+                            i--;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    
+        #region Randverbreiterung
+        /// <summary>
+        /// Diese Methode ist dazu da, um den Modellrand (voll zu druckenden äußeren Bereich) verbreitern.
+        /// Der Parameter randBreite soll die gewünschte resultierende Breite dieses Bereiches sein.
+        /// </summary>
+        /// <param name="randBreite"></param>
+        public void randVerbreiterung(int randBreite)
+        {
+            //Voxel werden erst in eine Liste eingetragen, bevor sie zum Rand hinzugefügt werden
+            List<ushort[]> hinzufügendeVoxel = new List<ushort[]>();
+            /*Es werden immer benachbarte Voxel hinzugefügt.
+            Um die gewünschte Breite zu erreichen, muss der Vorgang entsprechend oft durchgeführt werden.*/
+            for (int i = 0; i < randBreite - 1; i++)
+            {
+                //Die Verwendung einer foreach Schleife ist hier nicht möglich, da die Koodidaten des zu betrachtenden Voxels benötigt werden.
+                foreach (Voxel voxel in m_Voxelmatrix)
+                {
+                    ushort x = voxel.getKoords()[0];
+                    ushort y = voxel.getKoords()[1];
+                    ushort z = voxel.getKoords()[2];
+                    /*Es sind nur Voxel relevant, die zum Rand gehören.
+                    Leere Voxel müssen vorher aussortiert werden, weil wir nicht auf Attribute leerer Objekte zugreifen können.
+                    Die zweite Bedingung wird nicht ausgeführt, wenn die erste nicht erfüllt ist -> Fehlervermeidung*/
+                    if (voxel != null && voxel.getModellrand() == true)
+                    {
+                        /*Jeder Voxel hat 6, 18 oder 26 Nachbarn, je nach dem ob man Nachbarschaften über Flächen, Kanten und/oder Ecken als Nachbarschaft ansieht.
+                         Die Programmierung ist auf 26 ausgelegt.
+                         Bei jedem möglichen Nachbar ist zu prüfen, ob dieser im Modell liegt(nicht out of range) und existiert (nicht null)
+                         Die Prüfung wurde in eine neue Methode ausgelagert. Sie wird mit allen Voxeln aufgerunfen, bei denen sich jede Koodinate, um max 1 unterscheiden.*/
+                        foreach(Voxel v in getNeighbors(voxel))
+                        {
+                            if(v.getModellrand() == false)
+                            {
+                                hinzufügendeVoxel.Add(v.getKoords());
+                            }
+                        }
+                    }
+                }
+                //Damit das Verändern von Voxeln sich erst auf die nächste Iteration auswirkt, dürfen die Voxel nicht direkt verändert werden
+                foreach (ushort[] pos in hinzufügendeVoxel)
+                {
+                    m_Voxelmatrix[pos[0], pos[1], pos[2]].setModellrand(true);
+                }
+                hinzufügendeVoxel.Clear();
+            }
+        }
+        /// <summary>
+        /// Diese Methode übergibt alle existierenden Nachbarn eines existierenden Voxels
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public List<Voxel> getNeighbors(Voxel a)
+        {
+            int x = a.getKoords()[0];
+            int y = a.getKoords()[1];
+            int z = a.getKoords()[2];
+            List<Voxel> nachbarn = new List<Voxel>();
+            for (int x_div = -1; x_div <= 1; x_div++)
+            {
+                for (int y_div = -1; y_div <= 1; y_div++)
+                {
+                    for (int z_div = -1; z_div <= 1; z_div++)
+                    {
+                        try
+                        {
+                            if (a.IsNeighbor26(m_Voxelmatrix[x + x_div, y + y_div, z + z_div]))//nicht null)
+                            {
+                                nachbarn.Add(m_Voxelmatrix[x + x_div, y + y_div, z + z_div]);
+                            }
+                        }
+                        catch (IndexOutOfRangeException) {
+
+                        }
+                    }
+                }
+            }
+            return nachbarn;
+        }
+        #endregion
+        #region Infillmuster
+        public void Infillmustererzeugen(bool[,,] Musterbox)
+        {
+            //Gehe alle Voxel durch
+            foreach (List<Voxel> schicht in m_Schichten)
+            {
+                for (int i = 0; i<schicht.Count(); i++)
+                {
+                    Voxel v = schicht[i];
+                    //Voxel soll existieren und kein Modellrandvoxel sein, da diese immer alle gedruckt werden
+                    if (v != null && v.getModellrand() != true)
+                    {
+                        //Falls an der Voxel nicht zum Muster gehört
+                        int x = v.getKoords()[0], y = v.getKoords()[1], z = v.getKoords()[2];
+                        if (Musterbox[x, y, z] == false)
+                        {
+                            //Lösche diesen Voxel aus dem Modell
+                            m_Voxelmatrix[x, y, z] = null;
+                            //und aus der Schicht, weil das Löschen aus dem Modell scheinabr nicht ausreicht
+                            schicht.Remove(v);
+                        }
+                    }
+                }
+            }
+        }
+    #endregion
+    }
+   
+}
